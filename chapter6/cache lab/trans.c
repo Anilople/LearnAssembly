f4e20e6157b4403e213a606869c81af6599b1a31
Anilople <Anilople@outlook.com>
2018-05-15 23:58:34 +0800

    -M 32 -N 32 is wrong

---
+++ chapter6/cache lab/trans.c
@@ -0,0 +1,117 @@
+/* 
+ * trans.c - Matrix transpose B = A^T
+ *
+ * Each transpose function must have a prototype of the form:
+ * void trans(int M, int N, int A[N][M], int B[M][N]);
+ *
+ * A transpose function is evaluated by counting the number of misses
+ * on a 1KB direct mapped cache with a block size of 32 bytes.
+ */ 
+#include <stdio.h>
+#include "cachelab.h"
+
+int is_transpose(int M, int N, int A[N][M], int B[M][N]);
+
+/* 
+ * transpose_submit - This is the solution transpose function that you
+ *     will be graded on for Part B of the assignment. Do not change
+ *     the description string "Transpose  submission", as the driver
+ *     searches for that string to identify the transpose function to
+ *     be graded. 
+ */
+char transpose_submit_desc[] = "Transpose submission";
+void transpose_submit(int M, int N, int A[N][M], int B[M][N])
+{
+	int i, j, tmp;
+
+	int A_i_Base, A_j_Base, B_i_Base, B_j_Base;
+	A_i_Base = 0;
+	B_j_Base = 0;
+
+#define stride 8
+	while (A_i_Base < N)
+	{
+		A_j_Base = 0;
+		B_i_Base = 0;
+		while (A_j_Base < M)
+		{
+			//printf("Ai:%d\tAj:%d\tBi:%d\tBj:%d\n",A_i_Base,A_j_Base,B_i_Base,B_j_Base);
+			for (i = 0; i < stride; i++)
+			{
+				if (i + A_i_Base >= N || i + B_j_Base >= N) break; // about i
+				for (j = 0; j < stride; j++)
+				{
+					if (j + A_j_Base >= M || j + B_i_Base >= M) break; // about j
+					tmp = A[i + A_i_Base][j + A_j_Base];
+					B[j + B_i_Base][i + B_j_Base] = tmp;
+					//cout << "i + A_i_Base:" << i + A_i_Base << "\t" << "j + A_j_Base:" << j + A_j_Base << endl;
+					//cout << "j + B_i_Base:" << j + B_i_Base << "\t" << "i + B_j_Base:" << i + B_j_Base << endl;
+				}
+			}
+			// to next matrix block
+			A_j_Base += stride;
+			B_i_Base += stride;
+		}
+		A_i_Base += stride;
+		B_j_Base += stride;
+	}
+}
+
+/* 
+ * You can define additional transpose functions below. We've defined
+ * a simple one below to help you get started. 
+ */ 
+
+/* 
+ * trans - A simple baseline transpose function, not optimized for the cache.
+ */
+char trans_desc[] = "Simple row-wise scan transpose";
+void trans(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }    
+
+}
+
+/*
+ * registerFunctions - This function registers your transpose
+ *     functions with the driver.  At runtime, the driver will
+ *     evaluate each of the registered functions and summarize their
+ *     performance. This is a handy way to experiment with different
+ *     transpose strategies.
+ */
+void registerFunctions()
+{
+    /* Register your solution function */
+    registerTransFunction(transpose_submit, transpose_submit_desc); 
+
+    /* Register any additional transpose functions */
+    registerTransFunction(trans, trans_desc); 
+
+}
+
+/* 
+ * is_transpose - This helper function checks if B is the transpose of
+ *     A. You can check the correctness of your transpose by calling
+ *     it before returning from the transpose function.
+ */
+int is_transpose(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; ++j) {
+            if (A[i][j] != B[j][i]) {
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
